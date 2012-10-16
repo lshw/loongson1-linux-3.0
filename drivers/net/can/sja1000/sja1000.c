48a293676651622d606bf1be55be78982b5792a9
tanghaifeng <tanghaifeng-gz@loongson.cn>
2012-10-16 18:55:03 +0800

    修改sja1000 CAN控制器进入工作模式的代码,兼容LS1A/1B CAN控器

--- drivers/net/can/sja1000/sja1000.c
+++ drivers/net/can/sja1000/sja1000.c
@@ -153,7 +153,11 @@ static void set_normal_mode(struct net_device *dev)
 		}
 
 		/* set chip to normal mode */
+	#if defined(CONFIG_LS1X_CAN0) || defined(CONFIG_LS1X_CAN1)
+		priv->write_reg(priv, REG_MOD, MOD_STM);	/* LS1A/1B CAN operating mode */
+	#else
 		priv->write_reg(priv, REG_MOD, 0x00);
+	#endif
 		udelay(10);
 		status = priv->read_reg(priv, REG_MOD);
 	}
